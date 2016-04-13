1b3812846886d35c9e14be9a2f68c1e24482dad8
Lifirenk <lifirenk@gmail.com>
2016-04-13 11:42:07 -0400

    Added files via upload

--- HelpDeskFeedback.py
+++ HelpDeskFeedback.py
@@ -1,33 +1,71 @@
 #Help Desk Feedback Tool
 #Written by Andrew Slentz
 #Created: 3-23-16
-#Utilizes the exported email file for Footprints feedback to generate a customized report for the service desk.
+#Utilizes the exported email file for Footprints feedback to generate a 
+#customized report for the service desk.
 
-#import sys.stdout
+#4-5-16
+#added writing to file
+#defined keywords and locations to be removed
 
-dir = 'C:\\Feedback\\'
+dir = 'F:\\Feedback\\'
 file = 'feedback.csv'
 item = []
 report = []
+cont = []
+isa = []
+cont.append('https://footprints02')
+cont.append('Entered on')
 
+errata= ('Student','Studemp','Staff','Faculty','Admissions','Affiliate','Alumni','Retiree','(no data)','Excellent','Good','Poor')
 
+def popper( lusitania ):
+        while len(lusitania) > 0:
+                lusitania.pop()
+
+def stripper( candy ):
+        for sug in range(0, len(candy)):
+                for 
+               print candy[sug]
+        #line to strip of from known content
+        #strip from known context lines
+      #if in candy[1]:
+              
+
+#open read and close the file
 with open(dir+file, 'r') as openfo:
     rep = openfo.readlines()
 rep = [r.strip('\n') for r in rep]
 
+#only take from bottom of the ITEM to the DATE stamp
 
+#Detects the end of an item then determines if the item is of the type we are 
+#looking for (by length) filters out irrelevant information then adds to the
+#final output, ortherwise it assembles more info for the item
 for x in range(0, len(rep)):
-    print 'help 1'
-    while rep[x] != '________________________________':
-        print 'help 2'
-        item.append(rep[x])
-        print 'help 3'
-    print item[1]
-    print item[2]
-    report.append(item)
-    while len(item) > 0:
-        item.pop()
-print rep
+        if rep[x] == '________________________________':
+                if len(item) < 10:
+                        popper(item)
+                        continue
+                elif "HelpDesk" not in item[5]:
+                        popper(item)
+                        continue
+                if 'No Description Entered' in item[10]:
+                        popper(item)
+                        continue
+                stripper(item)
+                report.append(item)
+                for i in item:
+                        print 'main'
+                item.pop(1)
+                popper(item)
+        else:
+                item.append(rep[x])
 
-			
 
+#ext = open('F:\\Feedback\\LatestReport.txt', 'w+')
+#print report
+#for item in report:
+        #for l in item:
+                #print l#text.write(f)
+#text.close()
